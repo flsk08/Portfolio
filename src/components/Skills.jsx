b61ee8ca86738d16de59cb5414fbd8b3f80e8bff
Lina Ho <yenthi.ho.lina@gmail.com>
2022-04-05 22:21:01 +0200

    added skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Fade from "react-reveal/Fade";
 
 export default function Skills() {
+  const [skillsData, setSkillsData] = useState([]);
+
+  useEffect(() => {
+    const getSkills = async () => {
+      try {
+        const resp = await axios.get(
+          "http://localhost:8000/skills_and_tools"
+        );
+        console.log(resp.data);
+        setSkillsData(resp.data);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    };
+    getSkills();
+  }, []);
   return (
     <>
       <div className="skills-container-wrap">
-        <h2>Skills & Technologies</h2>
+        <div className="skills-title">
+          <Fade up>
+            <h2>Skills & Tools</h2>
+          </Fade>
+        </div>
+        <div className="skills-container">
+          {skillsData &&
+            skillsData.map((skill) => (
+              <Fade up>
+                <div key={skill.id} className="skill-set">
+                  <div className="icons">
+                    <img src={skill.icon} alt="icons" />
+                  </div>
+                  <div className="skills">
+                    <p>&nbsp;{skill.skill}</p>
+                  </div>
+                </div>
+              </Fade>
+            ))}
+        </div>
       </div>
-      <div className="skills-container"></div>
     </>
   );
 }
